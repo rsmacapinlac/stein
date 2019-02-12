57d18c9f78f7c421e201af13bfcdc1065231e801
Ritchie Macapinlac <rsmacapinlac@boogienet.com>
2019-02-11 21:14:48 -0800

    Updated with Rubocop style fixes

--- lib/stein/platforms/web/infinitewp.rb
+++ lib/stein/platforms/web/infinitewp.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'stein/platforms/web/web_automator'
 
 module Stein
@@ -5,7 +7,6 @@ module Stein
     module Web
       # Responsible for automating interactions with InfiniteWP
       class InfiniteWP < WebAutomator
-
         attr_accessor :_browser
         attr_accessor :_infinitewp_url
 
@@ -14,7 +15,8 @@ module Stein
 
           @_browser = Stein::Platforms::Web::Browser.new if @_browser.nil?
           @_browser.resize_to(1024, 800)
-          logger.debug "Initialized browser #{@_browser} #{@_browser.b.manage.window.size}"
+          logger.debug "Init browser: #{@_browser}"
+          logger.debug "Browser size: #{browser.b.manage.window.size}"
         end
 
         def browser
@@ -40,11 +42,7 @@ module Stein
         # Ensure that the sites menu is hidden, if it is already hidden then it
         # doesnt do anything.
         def hide_sites_menu
-          bottom_sites_showing = browser.get_element_by_id('bottom_sites_cont').
-            displayed?
-          if bottom_sites_showing
-            browser.get_element_by_class('showFooterSelector').click
-          end
+          browser.get_element_by_class('showFooterSelector').click if browser.get_element_by_id('bottom_sites_cont').displayed?
         end
 
         # Use the main menu to navigate IWP
@@ -68,50 +66,23 @@ module Stein
           sleep(1)
         end
 
-        def malware_scan(site)
-          open_site_selector('Protect', 'Malware Scan')
-
-          scan_modal = browser.wait.until do
-            browser.get_element_by_class('siteSelectorContainer')
-          end
-          logger.info "Malware site selector open? #{scan_modal.displayed?}"
-
-          scan_modal.find_element(link_text: site).click
-          browser.get_element_by_id('scanSucuri').click
-
-        end
-
-        def wordfence_scan(site)
-          open_site_selector('Protect', 'Wordfence')
-
-          scan_modal = browser.wait.until do
-            browser.get_element_by_class('siteSelectorContainer')
-          end
-          logger.info "Wordfence scan site selector open? #{scan_modal.displayed?}"
-
-          scan_modal.find_element(link_text: site).click
-          browser.get_element_by_id('scanWordfence').click
-        end
-
         def create_new_backup
           button = browser.wait.until do
             browser.b.find_element(id: 'restrictToggleCreateBackup')
           end
           browser.b.action.move_to(button).click(button).perform
+          logger.info 'Create new backup'
         end
 
         def select_site_in_modal(site)
           modal = browser.wait.until do
             test = browser.b.find_elements(class: 'dialog_cont')
-            if test.count == 0
-              sites_dialog = browser.get_element_by_class('website_items_cont')
+            if test.count.zero?
+              browser.get_element_by_class('website_items_cont')
             else
-              sites_dialog = test.first
+              test.first
             end
-            sites_dialog
           end
-          logger.info "Modal window open? #{modal.displayed?}"
-
           modal.find_element(link_text: site).click
           logger.info "Site selected: #{site}"
         end
@@ -119,6 +90,7 @@ module Stein
         def click_backup_details
           enter_details = browser.b.find_element(id: 'enterBackupDetails')
           browser.b.action.move_to(enter_details).click(enter_details).perform
+          logger.info 'Click backup details'
         end
 
         def add_backup_details(backup_name, backup_type = 'Phoenix (Beta)')
@@ -126,9 +98,11 @@ module Stein
             browser.b.find_element(id: 'backupName')
           end
           bu_name.send_keys backup_name
+          logger.info "Backup name: #{backup_name}"
 
           bu_type = browser.b.find_element(link_text: backup_type)
           browser.b.action.move_to(bu_type).click(bu_type).perform
+          logger.info "Backup type: #{backup_type}"
         end
 
         def start_backup
